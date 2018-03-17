d9c83849319fde6d9fab418c5ab6bbb452c3c77d
SiuLaw <lawsiufung@me.com>
2018-03-17 22:08:22 +0000

    remade classes; added stein stein

--- vanilla.cpp
+++ vanilla.cpp
@@ -116,16 +116,17 @@ void display_section_split() {
 Normal normal(Custom);
 
 
-//class
-class vanilla_option{
-    
+//base class
+class base_option{
+protected:
     double s0;
     double K;
     double r;
-    double mu;
     double gamma;
     double v0;
     bool is_call;
+    
+    string model;
     
     unsigned int N; // number of time partitions
     
@@ -147,8 +148,37 @@ class vanilla_option{
     double cal_Y2( double X1, double X2, double rho ) {
         return rho * X1 + pow(1-rho*rho,0.5) * X2;
     }
-
-    void HW_fd_pricing(double rho, double T, vector<double>& res) {
+    
+    double cal_P_BS(double T, double v0_fut) {
+        double d1 = ( log(s0 / K) + ( r + pow(v0_fut,2.0) / 2.0 ) * T ) / ( v0_fut * pow(T,0.5) );
+        double d2 = d1 - v0_fut * pow( T,0.5);
+        
+        // cout << "d1 = " << d1 << endl;
+        // cout << "d2 = " << d2 << endl;
+        
+        return normalCDF(-d2) * K * exp( -r*T) - s0 * normalCDF(-d1);
+    }
+    
+    double H(double T, double v0_fut, double d_plus, double d_mins ) {
+        // Since H is model independent, define as a function
+        double X_0 = log( s0 );
+        return exp( X_0 ) / ( pow( v0_fut, 2.0 ) * T * pow( 2.0 * M_PI, 0.5 )  ) * exp( - pow(d_plus, 2.0)/2.0 ) * ( - d_mins );
+    }
+    
+    double J(double T, double v0_fut, double d_plus, double d_mins ) {
+        // Since J is model independent, define as a function
+        double X_0 = log( s0 );
+        return exp( X_0 ) / ( pow( v0_fut * pow( T, 0.5 ), 3.0 ) * pow( 2.0 * M_PI, 0.5 ) ) * exp( - pow(d_plus, 2.0)/2.0 ) * ( d_plus * d_mins - 1.0 );
+    }
+    
+    virtual void print_model_para() {
+        cout << "   ... no additional model para specified" << endl;
+    }
+    
+    virtual double v_increment(double v, double Z, double dt, double dt_sqrt) {};
+    virtual double s_increment(double s, double W, double dt, double dt_sqrt, double v) {};
+    
+    void fd_pricing(double rho, double T, vector<double>& res, bool display_flag) {
         // prameter initlisation
         clock_t c;
         double duration;
@@ -156,10 +186,13 @@ class vanilla_option{
         double dt_sqrt = pow(dt, 0.5); // calculate fix value
         double discount = exp(-r * T); // discouting factor
         
-        double this_price, this_price_anti; // itermediate price results
+        double this_price; // itermediate price results
         double price_sum = 0;
         double price_sum_sqr = 0;
         double price;
+        
+        int percent;
+        int freq_display = 20;
         
         
         double s = s0; // intermediate price
@@ -185,9 +218,8 @@ class vanilla_option{
                 W = cal_Y1( X1[j], X2[j], rho);
                 Z = cal_Y2( X1[j], X2[j], rho);
                 
-                v += mu * v * dt + gamma * v * W * dt_sqrt;
-                s += r * s * dt + pow(v,0.5) * s * Z * dt_sqrt;
-                
+                v += v_increment(v, W, dt, dt_sqrt);
+                s += s_increment(s, Z, dt, dt_sqrt, v);
             }
             
             this_price = pay_off( s );
@@ -201,31 +233,38 @@ class vanilla_option{
             if( M % 1000 == 0 ) {
                 var = ( price_sum_sqr / M  - pow( price_sum / M , 2 ) ) / M;
                 tol = discount * price_sum / M / 100 / 1000;
-                cout << "M = " << M << "; " << (int)(tol/var * 100)  << "% ; price_est = " << discount * price_sum / M  << endl;
+                percent = (int)(tol/var * 100);
+                if(display_flag && percent % freq_display == 0 ) {
+                    cout << "M = " << M << "; " << percent << "% ; price_est = " << discount * price_sum / M  << endl;
+                }
+                
             }
         }
         
-        cout << "tol = " << tol << " reached after M = " << M << endl;
+        if(display_flag) {
+            cout << "tol = " << tol << " reached after M = " << M << endl;
+        }
         
         // record time
         duration = (clock() - c) / (double)CLOCKS_PER_SEC;
         price = (discount * price_sum / M );
-
+        
         res.push_back(price); // price
         res.push_back(duration); //time
     }
     
-    double HW_vt(double T) {
+    double vt(double T) {
         // prameter initlisation
         clock_t c;
         double duration;
         double dt = (double)T / N; // calculate fix value
         double dt_sqrt = pow(dt, 0.5); // calculate fix value
         
-        double this_price, this_price_anti; // itermediate price results
-        double price_sum = 0;
-        double price_sum_sqr = 0;
-        double price;
+        double this_price; // itermediate price results
+        double vol_sum = 0;
+        double vol_sum_sqr = 0;
+        double vol;
+        double this_vol_sum = 0;
         
         double v = v0;
         
@@ -236,76 +275,40 @@ class vanilla_option{
         
         c = clock();
         while( var > tol ) {
-            
             M += 1;
             
             vector<double> X1 = normal.generate(N); //generate normal vector of size N
             
-            this_price = 0;
             for (unsigned int j = 0;j < N;j++) {
                 W = X1[j];
-                v += mu * v * dt + gamma * v * W * dt_sqrt;
-                this_price += v;
+                v += v_increment(v, W, dt, dt_sqrt);
+                this_vol_sum += pow(v,2.0);
             }
-            price_sum     += this_price / N;
-            price_sum_sqr += pow( this_price / N, 2 );
+            vol_sum     += this_vol_sum;
+            vol_sum_sqr += pow( this_vol_sum, 2 );
             
-            v = v0; // rets path
+            // reset parameters for next loop
+            v = v0; // resets initial v0
+            this_vol_sum = 0; // resets sum of volatility
             
             if( M % 1000 == 0 ) {
-                var = ( price_sum_sqr / M  - pow( price_sum / M , 2 ) ) / M;
-                tol = price_sum / M / 100 / 1000;
-                cout << "M = " << M << "; " << (int)(tol/var * 100)  << "% ; future_average_vol = " << price_sum / M  << endl;
+                var = ( vol_sum_sqr / M  - pow( vol_sum / M , 2 ) ) / M;
+                tol = vol_sum / M / 100 / 1000;
+                // cout << "M = " << M << "; " << (int)(tol/var * 100)  << "% ; future_average_vol = " << pow( vol_sum / M / N , 0.5 ) << endl;
             }
         }
         
         // cout << "tol = " << tol << " reached after M = " << M << endl;
         
         // record time
-        return price_sum / M;
+        return pow( vol_sum / M / N , 0.5 );
         
     }
     
-    double cal_P_BS(double T, double v0_fut) {
-        double d1 = ( log(s0 / K) + ( r + pow(v0_fut,2.0) / 2.0 ) * T ) / ( v0_fut * pow(T,0.5) );
-        double d2 = d1 - v0_fut * pow( T,0.5);
-        
-        // cout << "d1 = " << d1 << endl;
-        // cout << "d2 = " << d2 << endl;
-        
-        return normalCDF(-d2) * K * exp( -r*T) - s0 * normalCDF(-d1);
-    }
+    virtual double NW(double T) {};
+    virtual double NN(double T) {};
     
-    double H(double T, double v0_fut, double d_plus, double d_mins ) {
-        // Since H is model independent, define as a function
-        double X_0 = log( s0 );
-        return exp( X_0 ) / ( pow( v0_fut, 2.0 ) * T * pow( 2.0 * M_PI, 0.5 )  ) * exp( - pow(d_plus, 2.0)/2.0 ) * ( - d_mins );
-    }
-    
-    double J(double T, double v0_fut, double d_plus, double d_mins ) {
-        // Since J is model independent, define as a function
-        double X_0 = log( s0 );
-        return exp( X_0 ) / ( pow( v0_fut * pow( T, 0.5 ), 3.0 ) * pow( 2.0 * M_PI, 0.5 ) ) * exp( - pow(d_plus, 2.0)/2.0 ) * ( d_plus * d_mins - 1.0 );
-    }
-    
-    double HW_NW(double T) {
-        double term_1 = -8.0 * gamma * pow( v0, 1.5 ) / mu;
-        double term_2 = ( exp( 1.5 * mu * T + 9.0/8.0 * pow( gamma, 2.0) * T ) - 1.0 ) / ( 3.0 * ( 3.0 * pow(gamma,2.0) + 4.0 * mu )  );
-        double term_3 = exp( mu*T ) * ( 1.0 - exp( 9.0/8.0 * pow(gamma,2.0) * T + 0.5 * mu * T ) ) / ( 9.0 * pow(gamma,2.0) + 4.0 * mu );
-        
-        return term_1 * ( term_2 + term_3 );
-    }
-    
-    double HW_NN(double T) {
-        double term_1 = -pow( v0, 2.0 );
-        double term_2 = pow( gamma, 2.0 ) * ( 1.0 - exp(mu*T) ) * ( 1.0 - 3.0 * exp( mu * T ) ) / ( mu * ( 3.0 * pow(gamma,2.0) + mu ) * ( 3.0 * pow(gamma,2.0) + 2.0*mu ) );
-        double term_3 = 3.0 * pow( gamma, 4.0 ) * pow( 1.0 - exp( mu * T ), 2.0 ) / ( pow(mu,2.0) * ( 3.0 * pow(gamma,2.0) + mu ) * ( 3.0 * pow(gamma,2.0) + 2.0* mu ) );
-        double term_4 = 2.0 * exp( 2.0 * mu * T ) * ( 1.0 - exp( 3.0 * pow(gamma,2.0) * T )) / ( 3.0 * ( 3.0 * pow(gamma,2.0) + mu ) * ( 3.0 * pow(gamma,2.0) + 2.0 * mu ) );
-        
-        return term_1 * ( term_2 + term_3 + term_4 );
-    }
-    
-    void HW_ap_pricing(double rho, double T, vector<double> &res ) {
+    void ap_pricing(double rho, double T, vector<double> &res ) {
         // prameter initlisation
         clock_t c;
         double duration;
@@ -313,44 +316,20 @@ class vanilla_option{
         
         c = clock();
         
-        double v0_est = HW_vt(T);
+        double v0_est = vt(T);
         double v0_fut = v0_est;
         
         // cout << endl;
         // cout << "v0 = " << v0 << endl;
-        cout << "v0_est = " << v0_est << endl;
+        // cout << "v0_est = " << v0_est << endl;
         
         double P_BS = cal_P_BS(T, v0_fut );
         
         double d_plus = ( log(s0 / K ) + ( r + pow(v0_fut,2.0) / 2.0 ) * T ) / ( v0_fut * pow(T, 0.5) );
         double d_mins = d_plus - v0_fut * pow( T, 0.5 );
         
-        double first_bracket = rho * 0.5 * H(T,v0_fut,d_plus,d_mins) * HW_NW(T);
-        
-        /*
-        double first_bracket =
-        rho * gamma * ( 8.0 * pow( v0, 1.5 ) * s0 * exp( -pow(d_plus,2.0)/2.0 ) * d_mins ) / ( mu * pow( 2.0 * M_PI, 0.5 ) * pow( v0_fut, 2.0 ) * T )
-        * (
-           ( exp(1.5 * mu * T + 9.0/8.0 * pow(gamma, 2.0) * T) - 1.0 ) / ( 3.0 * ( 3.0 * pow(gamma,2.0) + 4.0 * mu ) )
-           + ( exp( mu * T ) ) / ( 9.0 * pow(gamma,2.0) + 4.0 * mu ) * ( 1.0 - exp( 9.0/8.0 * pow(gamma,2.0) * T + 0.5 * mu * T ) )
-           );
-        */
-        
-        
-        double second_bracket = 1.0/8.0 * J(T,v0_fut,d_plus,d_mins) * HW_NN(T);
-        
-        /*
-        double second_bracket =
-        - pow( gamma, 2.0 ) / 8.0
-        * ( s0 * exp( -pow(d_plus, 2.0)/2.0 ) * ( d_plus * d_mins - 1.0 ) ) / ( pow( 2.0 * M_PI, 0.5 ) * pow( v0_fut * pow( T, 0.5 ), 1.5 ) )
-        * pow( v0, 2.0 )
-        * (
-           (pow(gamma,2.0) * ( 1.0 - exp( mu * T ) ) * ( 1.0 - 3.0 * exp( mu * T ) ) ) / ( mu * ( 3.0 * pow(gamma,2.0) + mu ) * ( 3.0 * pow(gamma,2.0) + 2.0 * mu ) )
-           + ( 3.0 * pow(gamma,4.0) * pow( 1.0 - exp( mu * T ), 2.0 ) ) / ( pow(mu,2.0) * ( 3.0 * pow(gamma,2.0) + mu ) * ( 3.0 * pow(gamma,2.0) + 2.0 * mu ) )
-           + ( 2.0 * exp( 2.0 * mu * T ) * ( 1.0 - exp( 3.0 * pow(gamma,2.0) * T ) ) ) / ( 3.0 * ( 3.0 * pow( gamma,2.0) + mu ) * ( 3.0 * pow(gamma, 2.0) + 2.0 * mu ) )
-           );
-         */
-        
+        double first_bracket  = rho/2.0 * H(T,v0_fut,d_plus,d_mins) * NW(T);
+        double second_bracket = 1.0/8.0 * J(T,v0_fut,d_plus,d_mins) * NN(T);
         
         // record time
         duration = (clock() - c) / (double)CLOCKS_PER_SEC;
@@ -359,14 +338,12 @@ class vanilla_option{
         res.push_back(price); // price
         res.push_back(duration); //time
         
-        
-        cout << "P_BS = " << P_BS << endl;
+        // cout << "P_BS = " << P_BS << endl;
         
     }
 
 public:
-    // object constructor
-    vanilla_option(const string& type, double s0, double K, double r, double mu, double gamma, double v0, int N):s0(s0),K(K),r(r),mu(mu),gamma(gamma),v0(v0), N(N) {
+    base_option(const string& type, double s0, double K, double r, double gamma, double v0, int N):s0(s0),K(K),r(r),gamma(gamma),v0(v0), N(N) {
         if(icompare(type,"call")){
             is_call = true;
         } else if(icompare(type,"put")){
@@ -376,27 +353,26 @@ public:
             is_call = true;
         }
     }
-
-    // this function calculate the option price at time 0 and analytic statistics
+    
     vector<double> calculate_price(const string& method, double rho = -0.5, double T = 1.0, bool display_results = false) {
         
         // return containeer
         vector<double> res;
-
+        
         //Use selected method to calcuate c_0
-        if (icompare(method, "HW_fd")) {
-            HW_fd_pricing(rho, T, res);
-        } else if (icompare(method,"HW_ap")){
-            HW_ap_pricing(rho, T, res);
+        if (icompare(method, "finite_diff")) {
+            fd_pricing(rho, T, res, display_results);
+        } else if (icompare(method,"decomp_approx")){
+            ap_pricing(rho, T, res);
         } else {
             cout << "ERROR: method " << method << " not recognized, please choose another" << endl;
             display_results = false;
         }
-
+        
         string name = "PRICE";
         if(display_results) {
-            display_opening(name,method);
-
+            display_opening(name,method,model);
+            
             display_section_split();
             
             cout << "   T        = " << T << endl;
@@ -404,23 +380,122 @@ public:
             cout << "   s0       = " << s0 << endl;
             cout << "   K        = " << K << endl;
             cout << "   r        = " << r  << endl;
-            cout << "   mu       = " << mu  << endl;
             cout << "   gamma    = " << gamma  << endl;
             cout << "   v0       = " << v0  << endl;
-
-            if( !icompare(method,"analytic") ) {
+            cout << "   rho      = " << rho << endl;
+            if( !icompare(method,"ap") ) {
                 cout << "   N        = " << N << endl;
             }
+            print_model_para(); // additional parameters specific to the model
+            
             display_section_split();
             res_print(res);
             display_ending();
         }
-
+        
         return res;
-
+        
     }
 };
 
+
+/************************************************************************************************************************************************************************/
+
+// Hull White model
+class HW_option : public base_option{
+protected:
+    double mu;
+    void print_model_para() {
+        //  cout << "            = " << << endl;
+        cout << "   mu       = " << mu  << endl;
+    }
+    
+    double v_increment(double v, double W, double dt, double dt_sqrt) {
+        return mu * v * dt + gamma * v * W * dt_sqrt;
+    }
+    
+    double s_increment(double s, double Z, double dt, double dt_sqrt, double v) {
+        return r * s * dt + pow(v,0.5) * s * Z * dt_sqrt;
+    }
+    
+    double NW(double T) {
+        // PAGE 39
+        double term_1 = -8.0 * gamma * pow( v0, 1.5 ) / mu;
+        double term_2 = ( exp( 1.5 * mu * T + 9.0/8.0 * pow( gamma, 2.0) * T ) - 1.0 ) / ( 3.0 * ( 3.0 * pow(gamma,2.0) + 4.0 * mu )  );
+        double term_3 = exp( mu*T ) * ( 1.0 - exp( 9.0/8.0 * pow(gamma,2.0) * T + 0.5 * mu * T ) ) / ( 9.0 * pow(gamma,2.0) + 4.0 * mu );
+        
+        return term_1 * ( term_2 + term_3 );
+    }
+    
+    double NN(double T) {
+        // PAGE 40
+        double term_1 = -pow( v0, 2.0 );
+        double term_2 = pow( gamma, 2.0 ) * ( 1.0 - exp(mu*T) ) * ( 1.0 - 3.0 * exp( mu * T ) ) / ( mu * ( 3.0 * pow(gamma,2.0) + mu ) * ( 3.0 * pow(gamma,2.0) + 2.0*mu ) );
+        double term_3 = 3.0 * pow( gamma, 4.0 ) * pow( 1.0 - exp( mu * T ), 2.0 ) / ( pow(mu,2.0) * ( 3.0 * pow(gamma,2.0) + mu ) * ( 3.0 * pow(gamma,2.0) + 2.0* mu ) );
+        double term_4 = 2.0 * exp( 2.0 * mu * T ) * ( 1.0 - exp( 3.0 * pow(gamma,2.0) * T )) / ( 3.0 * ( 3.0 * pow(gamma,2.0) + mu ) * ( 3.0 * pow(gamma,2.0) + 2.0 * mu ) );
+        
+        return term_1 * ( term_2 + term_3 + term_4 );
+    }
+public:
+    HW_option(const string& type, double s0, double K, double r, double gamma, double v0, int N, double mu):base_option(type, s0, K, r, gamma, v0, N),mu(mu) {
+        // testing
+        model = "Hull White";
+    }
+};
+
+
+/************************************************************************************************************************************************************************/
+
+// Stein Stein model
+class SS_option : public base_option{
+protected:
+    double kappa;
+    double theta;
+    
+    void print_model_para() {
+        //  cout << "            = " << << endl;
+        cout << "   kappa    = " << kappa << endl;
+        cout << "   theta    = " << theta << endl;
+    }
+    
+    double v_increment(double v, double W, double dt, double dt_sqrt) {
+        return kappa*( theta - v ) * dt + gamma * W * dt_sqrt;
+    }
+    
+    double s_increment(double s, double Z, double dt, double dt_sqrt, double v) {
+        return r * s * dt + v * s * Z * dt_sqrt;
+    }
+    
+    double NW(double T) {
+        // PAGE 43
+        double term_1 = gamma / pow( kappa, 2.0 );
+        double term_2 = 0.5 * ( pow(theta,2.0) * ( 4.0*kappa*T - 9.0) + v0*(v0 + 4.0*theta) + pow(gamma,2.0) / kappa * ( kappa * T - 1.0 ) );
+        double term_3 = ( 2.0*theta *kappa*T*(theta-v0) + 2.0*theta * ( 3.0*theta - 2.0 * v0 ) ) * exp( -kappa * T );
+        double term_4 = 0.5 * ( pow(gamma,2.0)/kappa * ( kappa*T + 1.0 ) - (theta - v0 ) * ( 3.0*theta - v0 ) - 2.0 * kappa * T * pow( theta - v0, 2.0 )  ) * exp( -2.0*kappa*T );
+        
+        return term_1 * ( term_2 + term_3 + term_4 );
+    }
+    
+    double NN(double T) {
+        // PAGE 44
+        double term_1 = pow(gamma,2.0) / pow( kappa, 3.0 );
+        double term_2 = 0.5 * ( - 5.0 * pow(gamma,2.0)/( 4.0*kappa) + pow(v0,2.0) + 6.0 * theta * v0 - 19.0 * pow(theta, 2.0) + T * ( 8.0*pow(theta,2.0)*kappa + pow(gamma,2.0) ) );
+        double term_3 = ( 2.0*theta * ( 7.0*theta - 3*v0 ) + 4.0 * theta * ( theta - v0 ) * kappa * T ) * exp( -kappa*T );
+        double term_4 = ( 2.0*theta*( 2.0 *v0 - 3.0 *theta) + T * ( pow(gamma,2.0) - 2.0 * kappa * pow(theta-v0, 2.0) ) + pow(gamma,2.0)/(2.0*kappa) ) * exp( -2.0*kappa*T );
+        double term_5 = ( 2.0*theta*(theta - v0) ) * exp( -3.0*kappa*T );
+        double term_6 = 0.5 * ( pow(gamma,2.0) / ( 4.0 * kappa ) - pow( theta - v0, 2.0) ) * exp( -4.0*kappa*T );
+        
+        return term_1 * ( term_2 + term_3 + term_4 + term_5 + term_6 );
+    }
+public:
+    SS_option(const string& type, double s0, double K, double r, double gamma, double v0, int N, double kappa, double theta):base_option(type, s0, K, r, gamma, v0, N),kappa(kappa),theta(theta) {
+        // testing
+        model = "Stein Stein";
+    }
+};
+
+
+/************************************************************************************************************************************************************************/
 
 int main() {
     double s0 = 100;
@@ -430,17 +505,26 @@ int main() {
     double gamma = 0.1;
     double v0 = 0.04;
     
-    int N = 1000;
+    int N = 10;
     
     double rho = -0.5;
     double T = 0.125;
     
-    // create an vanilla option
-    vanilla_option opt("put",s0,K,r,mu,gamma,v0,N);
+    // create an option under Hull White model
+    // PAGE 46
+    HW_option HW_opt("put",s0,K,r,gamma,v0,N,mu);
+    HW_opt.calculate_price("finite_diff", rho, T, true);
+    HW_opt.calculate_price("decomp_approx", rho, T, true);
     
-    // opt.calculate_price("HW_fd", rho, T, true);
-    opt.calculate_price("HW_ap", rho, T, true);
+    double kappa = 4;
+    double theta = 0.2;
+    v0 = 0.2;
     
+    // create an option under Stein Stein model
+    // PAGE 49
+    SS_option SS_opt("put",s0,K,r,gamma,v0,N,kappa,theta);
+    SS_opt.calculate_price("finite_diff",rho,T,true);
+    SS_opt.calculate_price("decomp_approx",rho,T,true);
 
 	return 0;
 }
